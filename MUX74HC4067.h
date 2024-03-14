1b3046fa8b2b68fb047f43ef8564cea2fdb9683d
Gerald Cells <37741184+geraldcells18@users.noreply.github.com>
2024-03-14 13:52:57 +0800

    Update MUX74HC4067.h

--- MUX74HC4067.h
+++ MUX74HC4067.h
@@ -2,10 +2,10 @@
  * Name: MUX74HC4067
  * Author: Nick Lamprianidis { nlamprian@gmail.com }
  * Version: 1.0
- * Description: A library for interfacing the 74HC4067 
+ * Description: A library for interfacing the 74HC4067
  *              multiplexers/demultiplexers
  * Source: https://github.com/nlamprian/MUX74HC4067
- * License: Copyright (c) 2014 Nick Lamprianidis 
+ * License: Copyright (c) 2014 Nick Lamprianidis
  *          This library is licensed under the MIT license
  *          http://www.opensource.org/licenses/mit-license.php
  *
@@ -20,6 +20,7 @@
 
 #define ANALOG 0
 #define DIGITAL 1
+#define DIGITAL_FOR_PULSE_IN 2
 
 #define DISABLED 0
 #define ENABLED 1
@@ -28,25 +29,25 @@
 class MUX74HC4067
 {
 public:
-	MUX74HC4067(uint8_t en, int8_t s0, int8_t s1 = -1, int8_t s2 = -1, int8_t s3 = -1);
+    MUX74HC4067(uint8_t en, int8_t s0, int8_t s1 = -1, int8_t s2 = -1, int8_t s3 = -1);
     void setChannel(int8_t pin, uint8_t set = ENABLED);
     void enable();
     void disable();
     void signalPin(uint8_t sig, uint8_t mode, uint8_t type = DIGITAL);
     int16_t read(int8_t chan_pin = -1);
     uint8_t write(int8_t chan_pin, uint8_t data, int8_t type = -1);
-    
+
 private:
-    int8_t signal_pin_status;  // -1 - Not Applicable
-                               //  0 - Analog Mode
-                               //  1 - Digital Mode
-    uint8_t signal_mode;  // INPUT, OUTPUT, INPUT_PULLUP
+    // -1 - Not Applicable, 0 - Analog Mode, 1 - Digital Mode, 2 Digital Mode(pulseIn())
+    int8_t signal_pin_status;
+
+    uint8_t signal_mode; // INPUT, OUTPUT, INPUT_PULLUP
     uint8_t num_of_control_pins;
-    uint8_t enable_status;  // DISABLED, ENABLED
+    uint8_t enable_status; // DISABLED, ENABLED
     uint8_t enable_pin;
     int8_t signal_pin;
     int8_t control_pin[4];
     uint8_t current_channel;
 };
 
-#endif  // MUX74HC4067
+#endif // MUX74HC4067
